b08170521b9635b957970c5841a1b17c506f4af0
sadam <sadamabbasi58@gmail.com>
2021-07-15 20:23:18 +0500

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is a Web App to find the latest Dj and other Musical events</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  );
+}
